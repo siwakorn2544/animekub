30f5162773b657a2a1a7e89cfdf2cb12f541f649
siwakorn2544 <118192610+siwakorn2544@users.noreply.github.com>
2022-11-26 23:13:29 +0700

    backend done

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-# from flask import request
-import requests
+from flask_mysqldb import MySQL
+
 
 # configuration
 DEBUG = True
- 
+
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-response = requests.get("https://api.jikan.moe/v4/anime/")
- 
+# connect database
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Pete040101'
+app.config['MYSQL_DB'] = 'comment_animekub'
+mysql = MySQL(app)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-@app.route('/animekub', methods=['GET'])
+@app.route('/animekub', methods=['POST'])
 def ping_pong():
-    return jsonify(response.json())
-
-
- 
-# @app.route('/members', methods=['GET'])
-# def all_members():
-#     return jsonify({
-#         'status': 'success',
-#         'members': MEMBERS
-#     })
- 
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' INSERT INTO comment (animeid, personname, comment)VALUES(%s,%s,%s)''',(1,'Peng','สวยพี่น้อง'))
+    mysql.connection.commit()
+    cursor.close()
+    return {'msg':"เสร็จละ"}
 if __name__ == '__main__':
     app.run()
